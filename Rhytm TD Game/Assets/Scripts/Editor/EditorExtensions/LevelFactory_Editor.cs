d593040d86a67ad54e2a99c8f4b6595a1c4e5869
elvess65 <37631917+elvess65@users.noreply.github.com>
2021-02-09 15:10:47 +0200

    Level editor improvements

--- Rhytm TD Game/Assets/Scripts/Editor/EditorExtensions/LevelFactory_Editor.cs
+++ Rhytm TD Game/Assets/Scripts/Editor/EditorExtensions/LevelFactory_Editor.cs
@@ -15,6 +15,8 @@ namespace RhytmTD.Editor.EditorExtensions
 
             public bool IsFoldedOut = false;
             public OverrideTypes AmountOverride = OverrideTypes.None;
+            public OverrideTypes DamageOverride = OverrideTypes.None;
+            public OverrideTypes HPOverride = OverrideTypes.None;
         }
 
         private bool m_IsWavesFoldedOut = false;
@@ -25,7 +27,7 @@ namespace RhytmTD.Editor.EditorExtensions
             DrawLevel();
 
             if (GUI.changed)
-                ValidateData();
+                ValidateLevel();
         }
 
         void OnEnable()
@@ -39,63 +41,79 @@ namespace RhytmTD.Editor.EditorExtensions
             }
         }
 
-        void ValidateData()
+        void ValidateLevel()
         {
             LevelFactory castedTarget = (LevelFactory)target;
 
             int waveIndex = 0;
             foreach(Wave wave in castedTarget.Waves)
             {
-                int overrideAmountCounter = 0;
-                int overrideDamageCounter = 0;
-                int overrideHPCounter = 0;
-                foreach(Chunk chunk in wave.Chunks)
-                {
-                    if (!chunk.OverrideAmount)
-                    {
-                        chunk.EnemiesAmount = wave.EnemiesAmount;
-                    }
-                    else
-                    {
-                        overrideAmountCounter++;
-                    }
-
-
-                    if (!chunk.OverrideDamage)
-                    {
-                        chunk.MinDamage = wave.MinDamage;
-                        chunk.MaxDamage = wave.MaxDamage;
-                    }
-                    else
-                    {
-                        overrideDamageCounter++;
-                    }
-
-                    if (!chunk.OverrideHP)
-                    {
-                        chunk.MinHP = wave.MinHP;
-                        chunk.MaxHP = wave.MaxHP;
-                    }
-                    else
-                    {
-                        overrideHPCounter++;
-                    }
-                }
-
-                if (overrideAmountCounter == wave.Chunks.Count)
-                {
-                    m_WavesEditorData[waveIndex].AmountOverride = WaveEditorData.OverrideTypes.Full;
-                }
-                else if (overrideAmountCounter > 0)
-                {
-                    m_WavesEditorData[waveIndex].AmountOverride = WaveEditorData.OverrideTypes.Part;
-                }
-                else
-                {
-                    m_WavesEditorData[waveIndex].AmountOverride = WaveEditorData.OverrideTypes.None;
-                }
-
+                ValidateWave(wave, waveIndex);
                 waveIndex++;
+            }
+        }
+
+        void ValidateWave(Wave wave, int waveIndex)
+        {
+            int overrideAmountCounter = 0;
+            int overrideDamageCounter = 0;
+            int overrideHPCounter = 0;
+            foreach (Chunk chunk in wave.Chunks)
+            {
+                ValidateChunk(wave, chunk);
+
+                if (chunk.OverrideAmount)
+                    overrideAmountCounter++;
+
+                if (chunk.OverrideDamage)
+                    overrideDamageCounter++;
+
+                if (chunk.OverrideHP)
+                    overrideHPCounter++;
+            }
+
+            //Amount
+            if (overrideAmountCounter == wave.Chunks.Count)
+                m_WavesEditorData[waveIndex].AmountOverride = WaveEditorData.OverrideTypes.Full;
+            else if (overrideAmountCounter > 0)
+                m_WavesEditorData[waveIndex].AmountOverride = WaveEditorData.OverrideTypes.Part;
+            else
+                m_WavesEditorData[waveIndex].AmountOverride = WaveEditorData.OverrideTypes.None;
+
+            //Damage
+            if (overrideDamageCounter == wave.Chunks.Count)
+                m_WavesEditorData[waveIndex].DamageOverride = WaveEditorData.OverrideTypes.Full;
+            else if (overrideDamageCounter > 0)
+                m_WavesEditorData[waveIndex].DamageOverride = WaveEditorData.OverrideTypes.Part;
+            else
+                m_WavesEditorData[waveIndex].DamageOverride = WaveEditorData.OverrideTypes.None;
+
+            //HP
+            if (overrideHPCounter == wave.Chunks.Count)
+                m_WavesEditorData[waveIndex].HPOverride = WaveEditorData.OverrideTypes.Full;
+            else if (overrideHPCounter > 0)
+                m_WavesEditorData[waveIndex].HPOverride = WaveEditorData.OverrideTypes.Part;
+            else
+                m_WavesEditorData[waveIndex].HPOverride = WaveEditorData.OverrideTypes.None;
+        }
+
+        void ValidateChunk(Wave parentWave, Chunk chunk)
+        {
+            if (!chunk.OverrideAmount)
+            {
+                chunk.EnemiesAmount = parentWave.EnemiesAmount;
+            }
+
+            if (!chunk.OverrideDamage)
+            {
+                chunk.MinDamage = parentWave.MinDamage;
+                chunk.MaxDamage = parentWave.MaxDamage;
+            }
+
+            if (!chunk.OverrideHP)
+            {
+                chunk.MinHP = parentWave.MinHP;
+                chunk.MaxHP = parentWave.MaxHP;
             }
         }
 
@@ -279,30 +297,45 @@ namespace RhytmTD.Editor.EditorExtensions
         }
 
 
-        void DrawWaveProperties(LevelFactory.Wave wave, int waveIndex)
+        void PreWavePropertyDraw(WaveEditorData.OverrideTypes propertyOverride, out Color initColor)
         {
-            //Enemies Amount
+            GUI.enabled = propertyOverride != WaveEditorData.OverrideTypes.Full;
+            initColor = EditorStyles.label.normal.textColor;
 
-            GUI.enabled = m_WavesEditorData[waveIndex].AmountOverride != WaveEditorData.OverrideTypes.Full;
-
-            Color initColor = EditorStyles.label.normal.textColor;
-            if (m_WavesEditorData[waveIndex].AmountOverride == WaveEditorData.OverrideTypes.Part)
+            if (propertyOverride == WaveEditorData.OverrideTypes.Part)
                 EditorStyles.label.normal.textColor = Color.yellow;
+        }
 
-            wave.EnemiesAmount = EditorGUILayout.IntField("EnemiesAmount:", wave.EnemiesAmount);
-
+        void PostWavePropertyDraw(Color initColor)
+        {
             EditorStyles.label.normal.textColor = initColor;
             GUI.enabled = true;
 
             EditorGUILayout.Space();
+        }
+
+        void DrawWaveProperties(LevelFactory.Wave wave, int waveIndex)
+        {
+            //Enemies Amount
+            PreWavePropertyDraw(m_WavesEditorData[waveIndex].AmountOverride, out Color initColor);
+
+            wave.EnemiesAmount = EditorGUILayout.IntField("EnemiesAmount:", wave.EnemiesAmount);
+
+            PostWavePropertyDraw(initColor);
 
             //Damage
+            PreWavePropertyDraw(m_WavesEditorData[waveIndex].AmountOverride, out initColor);
+
             DrawMinMax("MinDamage:", ref wave.MinDamage, "MaxDamage", ref wave.MaxDamage);
-            EditorGUILayout.Space();
+
+            PostWavePropertyDraw(initColor);
 
             //HP
+            PreWavePropertyDraw(m_WavesEditorData[waveIndex].AmountOverride, out initColor);
+
             DrawMinMax("MinHP:", ref wave.MinHP, "MaxHP", ref wave.MaxHP);
-            EditorGUILayout.Space();
+
+            PostWavePropertyDraw(initColor);
         }
 
         void DrawChunkProperies(LevelFactory.Chunk chunk, LevelFactory.Wave parentWave, int chunkIndex)
