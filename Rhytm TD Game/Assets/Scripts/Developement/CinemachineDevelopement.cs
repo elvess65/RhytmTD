91b3e5946e77dfa18c0a997ce99cb48dab7b7c28
elvess65 <37631917+elvess65@users.noreply.github.com>
2021-02-25 19:25:42 +0200

    Cinemachine package and test scene

---
+++ Rhytm TD Game/Assets/Scripts/Developement/CinemachineDevelopement.cs
@@ -0,0 +1,26 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace RhytmTD.Developement
+{
+    public class CinemachineDevelopement : MonoBehaviour
+    {
+        public Transform Target;
+        public CinemachineVirtualCamera Brain;
+        public CinemachineVirtualCamera Default;
+        public CinemachineVirtualCamera Main;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+            }
+
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+               
+            }
+        }
+    }
+}
+
