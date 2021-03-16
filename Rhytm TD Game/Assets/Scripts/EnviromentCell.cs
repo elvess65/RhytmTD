744e817299a8c3455ef77934d84d5ce90ad55ebc
elvess65 <37631917+elvess65@users.noreply.github.com>
2021-03-16 22:26:26 +0200

    Enviroment

---
+++ Rhytm TD Game/Assets/Scripts/EnviromentCell.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviromentCell : MonoBehaviour
+{
+    public Dictionary<int, Material> InitialMaterials;
+    public List<MeshRenderer> Renderers;
+}
---
+++ Rhytm TD Game/Assets/Scripts/EnviromentCell_Editor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EnviromentCell))]
+public class EnviromentCell_Editor : Editor
+{
+    private Material mat;
+
+    public override void OnInspectorGUI()
+    {
+        EnviromentCell castedTarget = (EnviromentCell)target;
+
+        if (GUILayout.Button("Cache renderers"))
+        {
+            if (castedTarget.InitialMaterials == null)
+                castedTarget.InitialMaterials = new Dictionary<int, Material>();
+
+            castedTarget.GetComponentsInChildren(castedTarget.Renderers);
+            foreach(MeshRenderer renderer in castedTarget.Renderers)
+            {
+                castedTarget.InitialMaterials[renderer.GetInstanceID()] = renderer.sharedMaterial;
+            }
+        }
+
+        if (GUILayout.Button("Restore materials"))
+        {
+            foreach (MeshRenderer renderer in castedTarget.Renderers)
+            {
+                renderer.sharedMaterial = castedTarget.InitialMaterials[renderer.GetInstanceID()];
+            }
+        }
+
+        if (GUILayout.Button("Set material"))
+        {
+            foreach (MeshRenderer renderer in castedTarget.Renderers)
+            {
+                renderer.sharedMaterial = mat;
+            }
+        }
+
+        mat = (Material)EditorGUILayout.ObjectField(mat, typeof(Material), false);
+
+        base.OnInspectorGUI();
+    }
+}
