b39dca4aec3aff6232db21ecb97fffde9b781ba1
Pavlo <elvess65@gmail.com>
2021-03-18 13:56:44 +0200

    Update EnviromentCellEditorMaterialSetup_Editor.cs

--- Rhytm TD Game/Assets/Scripts/EnviromentCellEditorMaterialSetup_Editor.cs
+++ Rhytm TD Game/Assets/Scripts/EnviromentCellEditorMaterialSetup_Editor.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class EnviromentCellEditorMaterialSetup_Editor : MonoBehaviour
+public class EnviromentCellEditorMaterialSetup_Editor : Editor
 {
     // Start is called before the first frame update
     void Start()
